57b767967531ab857a104642d6bd882ba2f6fb0f
Felix Tan <tan.felix@protonmail.com>
2020-12-31 02:02:10 -0500

    section 3, lect 46

---
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidBody;    // ref to Rigidbody of rocket
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();  // only acts of Rigidbody components; c# generic
+                                                // what if there are multiple Rigidbodys?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        // throw new NotImplementedException();
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);   // add forces relative to direction
+                                                      // behaviors changes w/ Rigidbody's mass
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.forward);  // Transform component of the Rocket
+                                                // forward = z-axis
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward); // counter-clockwise
+        }
+    }
+}
