0d97ebd75d6c0f9fdf446f7babadebfea0f4a18a
Felix Tan <tan.felix@protonmail.com>
2020-12-31 14:02:00 -0500

    section 3: lect 50

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -6,39 +6,59 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
     Rigidbody rigidBody;    // ref to Rigidbody of rocket
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();  // only acts of Rigidbody components; c# generic
                                                 // what if there are multiple Rigidbodys?
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Rotate()
     {
-        // throw new NotImplementedException();
+        rigidBody.freezeRotation = true;    // take manual control of rotation
 
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rigidBody.AddRelativeForce(Vector3.up);   // add forces relative to direction
-                                                      // behaviors changes w/ Rigidbody's mass
-        }
+        float rotationThisFrame = rcsThrust * Time.deltaTime;   // rot speed = thurst * frame_time
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(-Vector3.forward);  // Transform component of the Rocket
-                                                // forward = z-axis
+            transform.Rotate(-Vector3.forward * rotationThisFrame);  // Transform component of the Rocket
+                                                                    // forward = z-axis
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.forward); // counter-clockwise
+            transform.Rotate(Vector3.forward * rotationThisFrame); // counter-clockwise
+        }
+
+        rigidBody.freezeRotation = false;   // resume physics control of rotation
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);   // add forces relative to direction
+                                                      // behaviors changes w/ Rigidbody's mass
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play(); // prevent layering BRRRT sound   
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 }
