b71011d8da6a55ccb2510fccbd1463cd686afbef
Felix Tan <tan.felix@protonmail.com>
2021-01-01 01:51:24 -0500

    section 3: lect 57

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
 
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
     Rigidbody rigidBody;    // ref to Rigidbody of rocket
     AudioSource audioSource;
 
@@ -22,8 +25,11 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
     }
 
     private void Rotate()
@@ -62,18 +68,33 @@ public class Rocket : MonoBehaviour
         }
     }
 
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);  // go to level 2
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);  // go to level 1
+    }
+
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) { return; }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
                 break;
             case "Deadly":
-                print("Dead");
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 0f);
+                break;
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1f);    // load next scene after 1s
                 break;
             default:
-                print("?");
                 break;
         }
     }
