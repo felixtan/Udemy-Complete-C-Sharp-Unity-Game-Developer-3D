6f34af074f9d69c2c0ca804b6574cf0c11fc2527
Felix Tan <tan.felix@protonmail.com>
2021-01-04 02:29:20 -0500

    section 4: lect 85 and restart project with unity 2017 because of errors importing standard assets in 2019

---
+++ 4_Argon_Assault_2017/Assets/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // called before Start
+    void Awake()
+    {
+        DontDestroyOnLoad(this);	// keep game object alive
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ 4_Argon_Assault_2017/Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// refers to controller joystick
+		// maps a, d on keyboard to left, right on joystick
+		// value in [-1, 1]
+		float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		print(horizontalThrow);
+	}
+}
