e87538ad8597dd71691817280df5168efe6397a4
Felix Tan <tan.felix@protonmail.com>
2021-01-06 00:18:33 -0500

    section 4: lect 100

--- 4_Argon_Assault_2017/Assets/Scripts/CollisionHandler.cs
+++ 4_Argon_Assault_2017/Assets/Scripts/CollisionHandler.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour {
+
+	[Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+	[Tooltip("Death effect object")][SerializeField] GameObject deathFX;
 
 	// Use this for initialization
 	void Start () {
@@ -17,11 +21,18 @@ public class CollisionHandler : MonoBehaviour {
 	private void OnTriggerEnter(Collider other)
 	{
 		StartDeathSequence();
+		deathFX.SetActive(true);	// activate game object
+		Invoke("ReloadScene", levelLoadDelay);
 	}
 
 	private void StartDeathSequence()
 	{
 		print("Player dying");
 		SendMessage("OnPlayerDeath");
+	}
+
+	private void ReloadScene()
+	{
+		SceneManager.LoadScene(1);
 	}
 }
