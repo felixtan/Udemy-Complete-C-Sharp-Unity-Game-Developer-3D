e1ac2b9da1190b3d6e6cd5c62797841ee4644ab8
Felix Tan <tan.felix@protonmail.com>
2021-01-09 22:10:02 -0500

    section 4: lect 103

--- 4_Argon_Assault_2017/Assets/Scripts/Enemy.cs
+++ 4_Argon_Assault_2017/Assets/Scripts/Enemy.cs
@@ -6,7 +6,7 @@ public class Enemy : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		AddNonTriggerBoxCollider();
 	}
 	
 	// Update is called once per frame
@@ -14,6 +14,12 @@ public class Enemy : MonoBehaviour {
 		
 	}
 
+	// programatically add non-trigger box collider 
+	void AddNonTriggerBoxCollider()
+	{
+		Collider nonTriggerBoxCollider = gameObject.AddComponent<BoxCollider>();
+		nonTriggerBoxCollider.isTrigger = false;
+	}
 	void OnParticleCollision(GameObject other)
 	{
 		Destroy(gameObject);
