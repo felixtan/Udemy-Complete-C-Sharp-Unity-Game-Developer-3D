c60ca50c8439f590517f6da856cfa1207c471149
Felix Tan <tan.felix@protonmail.com>
2021-01-10 21:47:59 -0500

    section 5: lect 126

--- 5_Realm_Rush/Assets/CubeEditor.cs
+++ 5_Realm_Rush/Assets/CubeEditor.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 // by default, MonoBehaviours are only executed in Play Mode
 // this attribute tells callback functions to execute in edit mode as well
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
 	[Tooltip("Size of snap increments")]
 	[SerializeField] [Range(1f, 20f)]float gridSize = 10f;
 
+	TextMesh textMesh;
+
 	void Update () {
+		UpdatePosition();
+	}
+
+	void UpdatePosition()
+	{
 		Vector3 snapPos;
 		snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
 		// snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
 		snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 		transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+		textMesh = GetComponentInChildren<TextMesh>();
+		textMesh.text = (snapPos.x / gridSize) + "," + (snapPos.z / gridSize);
 	}
 }
