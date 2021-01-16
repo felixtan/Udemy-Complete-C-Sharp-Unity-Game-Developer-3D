2ac6399d25076e4d980b11f2e4769b254e25e144
Felix Tan <tan.felix@protonmail.com>
2021-01-16 02:14:52 -0500

    section 5: lect 149

---
+++ 5_Realm_Rush/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	[Range(0.1f, 5f)]
+	[SerializeField] float secondsBetweenSpawns = 5f;
+	[SerializeField] EnemyMovement enemyPrefab;
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(RepeatedlySpawnEnemies());
+	}
+	
+	IEnumerator RepeatedlySpawnEnemies()
+	{
+		while (true)
+		{
+			Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(secondsBetweenSpawns);
+		}
+	}
+}
--- 5_Realm_Rush/Assets/Scripts/Tower.cs
+++ 5_Realm_Rush/Assets/Scripts/Tower.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class Tower : MonoBehaviour {
 
 	[SerializeField] Transform objectToPan;
-	[SerializeField] Transform targetEnemy;
 	[SerializeField] float attackRange = 10f;
 	[SerializeField] ParticleSystem projectileParticle;
+	EnemyDamage targetEnemy;
 	
 	// Update is called once per frame
 	void Update () {
+		SetTargetEnemy();
+
 		if (targetEnemy)
 		{
 			// https://docs.unity3d.com/ScriptReference/Transform.LookAt.html
-			objectToPan.LookAt(targetEnemy);
+			objectToPan.LookAt(targetEnemy.transform);
 			FireAtEnemy();
 		}
 		else
@@ -43,6 +45,39 @@ public class Tower : MonoBehaviour {
 		if (projectileParticle != null) {
 			var emissionModule = projectileParticle.emission;
 			emissionModule.enabled = isActive;
+		}
+	}
+
+	private void SetTargetEnemy()
+	{
+		var sceneEnemies = FindObjectsOfType<EnemyDamage>();
+		if (sceneEnemies.Length == 0) { 
+			return;
+		}
+		else
+		{
+			EnemyDamage closestEnemy = sceneEnemies[0];
+
+			foreach (EnemyDamage testEnemy in sceneEnemies)
+			{
+				closestEnemy = GetClosest(closestEnemy, testEnemy);
+			}
+
+			targetEnemy = closestEnemy;
+		}
+	}
+
+	private EnemyDamage GetClosest(EnemyDamage enemyA, EnemyDamage enemyB)
+	{
+		float distToA = Vector3.Distance(enemyA.transform.position, gameObject.transform.position);
+		float distToB = Vector3.Distance(enemyB.transform.position, gameObject.transform.position);
+		if (distToA <= distToB)
+		{
+			return enemyA;
+		}
+		else
+		{
+			return enemyB;
 		}
 	}
 }
